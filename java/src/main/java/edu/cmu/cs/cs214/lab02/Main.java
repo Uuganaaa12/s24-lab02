88fd44631861f8df994dadeb733399483ecea403
uuganaa <uugnaauugaa56@gmail.com>
2025-02-14 17:35:06 +0800

    2.14

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         Rectangle rectangle = new Rectangle(2, 3);
         Circle circle = new Circle(4);
-        Square square = new Square(6.0);
+        Square square = new Square(4.0);
 
         Renderer renderer1 = new Renderer(rectangle);
         Renderer renderer2 = new Renderer(circle);
